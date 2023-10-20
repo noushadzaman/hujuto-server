c193c934ae42c8d6f33a22adf4f98a3bcaba3e0e
Noushadozi <noushadozi333@gmail.com>
2023-10-20 19:14:49 +0600

    dynamically retriving single data method added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let brands = require("./brands.json");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -23,7 +23,26 @@ async function run() {
   try {
     await client.connect();
 
-    
+    const vehicleCollection = client.db("hujutoDB").collection("vehicles");
+
+    app.post("/vehicle", async (req, res) => {
+      const user = req.body;
+      const result = await vehicleCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/vehicle", async (req, res) => {
+      const cursor = vehicleCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/vehicle/:id", async (req, res) => {
+      const id = req.params._id;
+      const query = { id: new ObjectId(id) };
+      const result = await vehicleCollection.findOne();
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
