d8381d184a77eead43fe99551b8b192a4c679617
Noushadozi <noushadozi333@gmail.com>
2023-12-16 23:13:53 +0600

    pagination

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.es2kkf9.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x4h5cla.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -21,22 +21,56 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    //  client.connect();
 
-    const vehicleCollection = client.db("hujutoDB").collection("vehicles");
+    const brandCollection = client.db("hujuto").collection("brands");
+    const vehicleCollection = client.db("hujuto").collection("vehicle");
     const cartProductCollection = client
-      .db("hujutoDB")
+      .db("hujuto")
       .collection("cartProducts");
 
-    app.post("/vehicle", async (req, res) => {
-      const vehicle = req.body;
-      const result = await vehicleCollection.insertOne(vehicle);
+    app.get("/brand", async (req, res) => {
+      const cursor = brandCollection.find();
+      const result = await cursor.toArray();
       res.send(result);
     });
 
+    app.get("/brand/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await brandCollection.findOne(query);
+      res.send(result);
+    });
+
+    // app.post("/vehicle", async (req, res) => {
+    //   const vehicle = req.body;
+    //   const result = await vehicleCollection.insertOne(vehicle);
+    //   res.send(result);
+    // });
+
     app.get("/vehicle", async (req, res) => {
-      const cursor = vehicleCollection.find();
-      const result = await cursor.toArray();
+      let query = {};
+      if (req.query.brandName) {
+        query = { brandName: req.query.brandName };
+      }
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      const result = await vehicleCollection
+        .find(query)
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+      res.send(result);
+    });
+
+    app.get("/vehicleCount", async (req, res) => {
+      let query = {};
+      if (req.query.brandName) {
+        query = { brandName: req.query.brandName };
+      }
+      const result = await vehicleCollection
+        .find(query)
+        .toArray();
       res.send(result);
     });
 
@@ -54,54 +88,57 @@ async function run() {
     });
 
     app.get("/cartProduct", async (req, res) => {
-      const cursor = cartProductCollection.find();
+      let query = {};
+      if (req.query.email) {
+        query = { userEmail: req.query.email };
+      }
+      const cursor = cartProductCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     });
 
-    app.get("/vehicleUpdate/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await vehicleCollection.findOne(query);
-      res.send(result);
-    });
+    // app.get("/vehicleUpdate/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) };
+    //   const result = await vehicleCollection.findOne(query);
+    //   res.send(result);
+    // });
 
-    app.put("/update/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const options = { upsert: true };
-      const updatedVehicle = req.body;
-      const vehicle = {
-        $set: {
-          imageUrl: updatedVehicle.imageUrl,
-          name: updatedVehicle.name,
-          brandName: updatedVehicle.brandName,
-          type: updatedVehicle.type,
-          price: updatedVehicle.price,
-          rating: updatedVehicle.rating,
-          shortDescription: updatedVehicle.shortDescription,
-        },
-      };
-      const result = await vehicleCollection.updateOne(query, vehicle, options);
-      res.send(result);
-    });
+    // app.put("/update/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) };
+    //   const options = { upsert: true };
+    //   const updatedVehicle = req.body;
+    //   const vehicle = {
+    //     $set: {
+    //       imageUrl: updatedVehicle.imageUrl,
+    //       name: updatedVehicle.name,
+    //       brandName: updatedVehicle.brandName,
+    //       type: updatedVehicle.type,
+    //       price: updatedVehicle.price,
+    //       rating: updatedVehicle.rating,
+    //       shortDescription: updatedVehicle.shortDescription,
+    //     },
+    //   };
+    //   const result = await vehicleCollection.updateOne(query, vehicle, options);
+    //   res.send(result);
+    // });
 
-    app.get("/cartProduct/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await cartProductCollection.findOne(query);
-      res.send(result);
-    });
+    // app.get("/cartProduct/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) };
+    //   const result = await cartProductCollection.findOne(query);
+    //   res.send(result);
+    // });
 
     app.delete("/cartProduct/:id", async (req, res) => {
       const id = req.params.id;
-      console.log(id);
       const query = { _id: new ObjectId(id) };
       const result = await cartProductCollection.deleteOne(query);
       res.send(result);
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
@@ -111,7 +148,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send(brands);
+  res.send(`hujuto running`);
 });
 
 app.listen(port, () => {
